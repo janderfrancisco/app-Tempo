19346154aa9f064947d1f20cd9a27c1ee3cdc9a0
Admin <admin@Mac-Admin.local>
2017-03-01 00:07:07 -0300

    Initial Commit

---
+++ rainyShine1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rainyShine1
+//
+//  Created by Admin on 01/03/17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
